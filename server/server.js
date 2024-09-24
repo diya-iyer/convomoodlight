f8e093fbb6ee88ce34feb6f0fa3954a9b8d46a26
Elia Cohen <mrc6113@rit.edu>
2024-09-24 01:39:57 -0400

    /end endpoint

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const crypto = require('crypto');
 const app = express()
+const port = 3000
 
 const sessions = {};
 
@@ -9,7 +10,19 @@ app.post('/new', (req, res) => {
   const id = crypto.randomUUID();
   sessions[id] = {};
 
-  res.status(201).json({id});
+  return res.status(201).json({id});
+});
+
+// Closes a mood light session.
+app.post('/end', (req, res) => {
+  const { id } = req.query;
+  console.log(id, req.query);
+
+  if (!id) return res.status(400).json({error: "Expected a session id. None was given."});
+  if (!sessions[id]) return res.status(404).json({error: "No session found with that id."});
+  
+  delete sessions[id];
+  return res.status(204).json({});
 });
 
 // get sentiment analysis
@@ -17,3 +30,6 @@ app.post('/sentiment', (req, res) => {
   console.log(req.body);
 });
 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
