a81bcc4f27201e7a6bd4cec667a4987766b9ced4
Elia Cohen <mrc6113@rit.edu>
2024-10-01 14:43:07 -0400

    update sentiment.js for changes to api code

--- server/sentiment.js
+++ server/sentiment.js
@@ -1,9 +1,14 @@
-const assemblyAI = require('./../api/apiclient');
+const assemblyAI = require('./../api/apiclient.ts');
 
 // sends the audio to the AI for analysis
 const doSentimentAnalysis = async (session, url) => {
-  const sentiment = await assemblyAI.transcribeAndAnalyzeSentiment(url);
-  session.sentiment = sentiment;
+  const result = await assemblyAI.transcribeAndAnalyzeSentiment(url);
+  if (!result.sentimentResults) return;
+  
+  session.sentiment = {
+    sentiment: result.sentimentResults.sentiment,
+    confidence: result.sentimentResults.confidence,
+  };
   session.sentimentReady = true;
 };
 
